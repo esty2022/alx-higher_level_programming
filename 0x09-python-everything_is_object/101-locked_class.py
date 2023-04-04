7d1ce1322fb5f4210f6e56d952897cd861ee79ae
esty2022 <esther.duntoye@gmail.com>
2023-04-04 09:20:20 -0700

    object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 101-locked_class.py
+# ALX SCHOOL
+"""Defines a locked class."""
+
+
+class LockedClass:
+    """
+    The first name.
+    """
+
+    __slots__ = ["first_name"]
+
