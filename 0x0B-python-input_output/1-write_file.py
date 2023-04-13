61fe82db81a94430983ac1a289cd09c854317346
esty2022 <esther.duntoye@gmail.com>
2023-04-13 10:15:00 -0700

    output

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this explains a file function."""
+
+
+def write_file(filename="", text=""):
+    
+    
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
+
---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""explain string-to-JSON function."""
+import json
+
+
+def to_json_string(my_obj):
+    """ for to Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
